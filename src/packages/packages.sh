25ae61fba18a31e140b063e7101a38fe7cecf53c
bahram-aliyev <bahrama@iqmetrix.com>
2019-07-21 15:16:14 -0600

    Update packages.sh

--- src/packages/packages.sh
+++ src/packages/packages.sh
@@ -4,7 +4,7 @@
 export PROGET_ROOT=/var/dev/proget
 export PROGET_PACKAGES=$PROGET_ROOT/packages
 export PROGET_EXTENSIONS=$PROGET_ROOT/extensions
-export PROGET_POSTGRES_DATA=$PROGET_ROOT/postgresql/data
+export PROGET_POSTGRESQL_DATA=$PROGET_ROOT/postgresql/data
 
 mkdir -p $PROGET_PACKAGES
 mkdir -p $PROGET_POSTGRES_DATA
@@ -14,8 +14,8 @@ export PROGET_HTTP_PORT=10180
 export PROGET_POSTGRES_PORT=10132
 
 # DB-POSTGRES
-export PROGET_POSTGRES_DATABASE=proget
-export PROGET_POSTGRES_USER=proget
-export PROGET_POSTGRES_PASSWORD=r00t
+export PROGET_POSTGRESQL_DATABASE=proget
+export PROGET_POSTGRESQL_USER=proget
+export PROGET_POSTGRESQL_PASSWORD=r00t
 
 docker-compose -f packages_docker_compose.yml up -d
