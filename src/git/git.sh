6a506764346cf3ffe501e8f4345d3af5991cf266
bahram-aliyev <bahrama@iqmetrix.com>
2019-07-21 15:54:02 -0600

    Update git.sh

--- src/git/git.sh
+++ src/git/git.sh
@@ -15,6 +15,8 @@ export GOGS_SSH_PORT=10022
 # DB-MYSQL
 export GOGS_MYSQL_PORT=10306
 export GOGS_MYSQL_DATABASE=gogs
+export GOGS_MYSQL_USER=gogs
+export GOGS_MYSQL_USER_PASSWORD=g09z
 export GOGS_MYSQL_PASSWORD=r00t
 
 docker-compose -f git_docker_compose.yml up -d
