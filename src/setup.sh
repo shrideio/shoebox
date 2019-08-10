ebaceb9e88d281b916bc50fc1055665aaf107128
Bahram Aliyev <aliyev2b@gmail.com>
2019-08-10 13:31:42 -0600

    fixed export assignments

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DEV_ROOT = /var/dev
-export SRC_ROOT = /tmp/shoebox/src
+export DEV_ROOT=/var/dev
+export SRC_ROOT=/tmp/shoebox/src
 
 echo "Started setup."
 echo "DEV_ROOT: $DEV_ROOT"
@@ -15,9 +15,9 @@ echo "Setting up Gogs..."
 echo "https://gogs.io/"
 echo
 
-export GOGS_ROOT = $DEV_ROOT/gogs
-export GOGS_DATA = $GOGS_ROOT/data
-export GOGS_MYSQL_DATA = $GOGS_ROOT/mysql/data
+export GOGS_ROOT=$DEV_ROOT/gogs
+export GOGS_DATA=$GOGS_ROOT/data
+export GOGS_MYSQL_DATA=$GOGS_ROOT/mysql/data
 
 mkdir -p $GOGS_DATA
 mkdir -p $GOGS_MYSQL_DATA
@@ -27,7 +27,7 @@ echo "GOGS_DATA: $GOGS_DATA"
 echo "GOGS_MYSQL_DATA: $GOGS_MYSQL_DATA"
 echo
 
-export GIT_SRC = $SRC_ROOT/git
+export GIT_SRC=$SRC_ROOT/git
 mv $GIT_SRC/env.tmpl GIT_SRC/.env
 find $GIT_SRC -type f -name '*.env' -exec sed -i -e 's|@GOGS_DATA|'"$GOGS_DATA"'|g' {} \;
 find $GIT_SRC -type f -name '*.env' -exec sed -i -e 's|@GOGS_MYSQL_DATA|'"$GOGS_MYSQL_DATA"'|g' {} \;
@@ -45,10 +45,10 @@ echo "Setting up ProGet..."
 echo "https://inedo.com/proget"
 echo
 
-export PROGET_ROOT = $DEV_ROOT/proget
-export PROGET_PACKAGES = $PROGET_ROOT/packages
-export PROGET_EXTENSIONS = $PROGET_ROOT/extensions
-export PROGET_POSTGRESQL_DATA = $PROGET_ROOT/postgresql/data
+export PROGET_ROOT=$DEV_ROOT/proget
+export PROGET_PACKAGES=$PROGET_ROOT/packages
+export PROGET_EXTENSIONS=$PROGET_ROOT/extensions
+export PROGET_POSTGRESQL_DATA=$PROGET_ROOT/postgresql/data
 
 mkdir -p $PROGET_PACKAGES
 mkdir -p $PROGET_EXTENSIONS
@@ -80,10 +80,10 @@ echo "https://www.vaultproject.io/"
 echo
 
 export VAULT_ROOT=$DEV_ROOT/vault
-export VAULT_CONFIG = $VAULT_ROOT/config
-export VAULT_LOGS = $VAULT_ROOT/logs
-export VAULT_CONSUL_CONFIG = $VAULT_ROOT/consul/config
-export VAULT_CONSUL_DATA = $VAULT_ROOT/consul/data
+export VAULT_CONFIG=$VAULT_ROOT/config
+export VAULT_LOGS=$VAULT_ROOT/logs
+export VAULT_CONSUL_CONFIG=$VAULT_ROOT/consul/config
+export VAULT_CONSUL_DATA=$VAULT_ROOT/consul/data
 
 mkdir -p $VAULT_CONFIG
 mkdir -p $VAULT_LOGS
