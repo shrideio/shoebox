1b5f9cd7205a28082e1291d7e55daa34d1296099
bahram-aliyev <bahram.aliyev@gmail.com>
2020-04-19 10:27:25 -0600

    Add private nuget repo support (#11)
    * added sample package project with binary
    
    * added documentation for pushing a nuget package to a private repository
    
    * added external package
    
    * added package source support for the build pipeline
    
    * correct default packages uri
    
    * Update Dockerfile
    
    corrected add nuget source command
    
    * Update Dockerfile
    
    corrected package source name extraction
    
    * wip
    
    * update docker-compose installation guide
    
    * fixed project failing on Docker build
    
    * fixed Docker build
    
    * minor corrections to compose files
    
    * request a wildcard certificate
    
    * added redis to project setup
    
    * added new host providers
    
    * updated project description, added redis into the containers list
    
    * corrected packages source url
    
    * Update packages_docker_compose.yml
    
    Co-authored-by: Bahram Aliyev <aliyev2b@gmail.com>

--- src/ci/ci.build.sample/build.bat
+++ src/ci/ci.build.sample/build.bat
@@ -1,6 +1,3 @@
-dotnet restore src/ci.build.sample
 dotnet build src/ci.build.sample
-
-dotnet restore tests/ci.build.sample.Tests
 dotnet build tests/ci.build.sample.Tests
 dotnet test tests/ci.build.sample.Tests
